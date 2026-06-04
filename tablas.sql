b70c6214c1f9928b6001be0ef88b06b716650af2
Santiago Leonel Monzon <slmonzon@fi.uba.ar>
2026-06-03 22:25:41 -0300

    Agrego tabla equipos

--- tablas.sql
+++ tablas.sql
@@ -3,3 +3,9 @@ create table pronosticos_deportivos(nro_pronostico int not null ,
                                    DNI int not null,
                                     primary key(nro_pronostico),
                                     foreign key (DNI) references usuarios(DNI));
+
+CREATE TABLE equipos(nombre_equipo VARCHAR(100),
+                     pais VARCHAR(50),
+                     estadio VARCHAR(100),
+                     PRIMARY KEY (nombre_equipo, pais)
+                    );
