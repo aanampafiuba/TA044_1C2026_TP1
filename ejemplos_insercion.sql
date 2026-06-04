e1493c846933013db87697d7de51764b1f06128f
Sebastian <scolazo@fi.uba.ar>
2026-06-04 10:41:43 -0300

    arreglo sintaxis de inserciones pronosticos_deportivos

--- ejemplos_insercion.sql
+++ ejemplos_insercion.sql
@@ -2,17 +2,17 @@
 
 --Inseciones para tabla pronosticos_deportivos
 
-insert into pronosticos_deportivos
-values ('1','Gana local', '45111222'),
-       ('2','Gana local', '45111333'),
-       ('3','Empate', '45111444'),
-       ('4','Gana visitante', '45111555'),
-       ('5','Gana local', '45111666'),
-       ('6','Empate', '45111777'),
-       ('7','Gana visitante', '45111888'),
-       ('8','Gana local', '45111999'),
-       ('9','Gana visitante', '45222111'),
-       ('10','Empate', '45222333');
+insert into pronosticos_deportivos(nro_pronostico, pronostico_resultado, DNI) values
+('1','Gana local', '45111222'),
+('2','Gana local', '45111333'),
+('3','Empate', '45111444'),
+('4','Gana visitante', '45111555'),
+('5','Gana local', '45111666'),
+('6','Empate', '45111777'),
+('7','Gana visitante', '45111888'),
+('8','Gana local', '45111999'),
+('9','Gana visitante', '45222111'),
+('10','Empate', '45222333');
 
 --agregue 10 ejemplos basicos, igual el campo del DNI deberia tener sentido
 --con los ejemplos de insercion de la tabla usuarios
