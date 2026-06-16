006e52bbae9cadcfaa593cdc604191a9a2710596
SebastianColazo-111737 <scolazo@fi.uba.ar>
2026-06-16 11:39:56 -0300

    Agrego al main las consultas de nazareno

--- nuevas_consultas.sql
+++ nuevas_consultas.sql
@@ -1,7 +1,13 @@
 
+--Registrar un torneo. (nazareno)
+INSERT INTO torneos (nombre_torneo, pais, fecha_inicio, fecha_final) 
+VALUES ('Torneo Clausura 2026', 'Argentina', '2026-08-01', '2026-12-15');
+
+-- Listar todos los usuarios de la aplicación. ( nazareno )
+SELECT DNI, nombre, fecha_nacimiento, email, pais
+FROM usuarios;
 
 --Registrar un resultado de un partido (Sebastian):
-
 UPDATE juega_un
 SET goles = 2
 WHERE id_partido = 1 AND rol = 'Local';
@@ -11,21 +17,18 @@ SET goles = 1
 WHERE id_partido = 1 AND rol = 'Visitante';
 
 --Contabilizar la cantidad de usuarios, agrupados por pais (Sebastian):
-
 select pais, count(*) as cantidad_usuarios_por_pais
 from usuarios
 group by pais
 order by cantidad_usuarios_por_pais desc;
 
 -- Eliminar un torneo de (Sebastian):
-
 delete
 from torneos
 where nombre_torneo = 'Copa Argentina';
 
 
 --Desregistrar a un usuario de la aplicación (dar un ejemplo). (Sebastian)
-
 INSERT INTO usuarios(DNI, nombre, fecha_nacimiento, email, pais)
 VALUES (75888999, 'Don Ramon', '1924-07-02', 'donramon@gmail.com', 'Argentina');
 
